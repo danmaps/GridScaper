5a5bb8936c072526e46750eaf85bf17599670e82
danmaps <dannybmcvey@gmail.com>
2025-05-07 15:31:28 -0700

    init

---
+++ utils/traverseObject.js
@@ -0,0 +1,15 @@
+/**
+ * Recursively traverse json object
+ * @param {object} target
+ * @param {function} callback
+ */
+function traverseObject(target, callback) {
+  callback(target);
+  if (typeof target === 'object') {
+    for (let key in target) {
+      traverseObject(target[key], callback);
+    }
+  }
+}
+
+export default traverseObject;
